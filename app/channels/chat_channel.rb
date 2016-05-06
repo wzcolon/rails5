cf442412f47bf28881263ba8fef7ec3aec378ff8
Zach Colon <zcolon80@gmail.com>
2016-05-06 17:13:25 -0500

    ActionCable Chat app

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from 'messages'
+  end
+
+  def speak(data)
+    ActionCable.server.broadcast('messages',
+      message: render_message(data['message']))
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(partial: 'messages/message',
+                                 locals: { message: message, username: current_user })
+  end
+end
